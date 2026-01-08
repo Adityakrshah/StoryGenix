8098b735ffe94dcb0a1d2346bf9e518bf32b1d18
Adityakrshah <bittuadi4@gmail.com>
2026-01-08 19:02:03 +0530

    [vite config file]

--- cinematic-story-frontend/vite.config.js
+++ cinematic-story-frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    historyApiFallback: true
-  },
-  preview: {
-    historyApiFallback: true
-  }
+  plugins: [react(), tailwindcss()],
 });
